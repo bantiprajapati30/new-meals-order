2351fbe4e13dc43d3edc410efb4f0dd27c93b4a7
Banti Prajapati <banti.prajapati@systematixindia.com>
2021-07-27 19:29:51 +0530

    27-07-2021

--- src/component/Meals/mealItem/MealItemForm.js
+++ src/component/Meals/mealItem/MealItemForm.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import {useRef, useState} from 'react'
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 
 function MealItemForm(props) {
 
+ const amountInputRef=useRef();
+ const [amountIsValid, setAmountIsValid]=useState(true);
+
      const submitHandler=event=>{
          event.preventDefault();
+         const enteredAmount=amountInputRef.current.value;
+         const enteredAmountNumber= + enteredAmount;
+         if(enteredAmount.trim().length===0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+             setAmountIsValid(false);
+             console.log(amountIsValid);
+             return;
+           
+         }
+         props.onAddToCart(enteredAmountNumber);
     }
     return (
-       <form className={classes.form}>
-           <Input label="Ammount" 
+       <form className={classes.form} onSubmit={submitHandler}>
+           <Input 
+           ref={amountInputRef}
+           label="Amount" 
            input={{
             id:'amount_'+props.id,
             type: 'number',
@@ -20,6 +34,7 @@ function MealItemForm(props) {
             defaultValue: '1'
            }}/>
            <button>+ Add</button>
+           {!amountIsValid && <p>Please add some values netween 1 to 5</p>}
        </form>
     )
 }
